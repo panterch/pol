7fad4a50011e395bd55c642fb84e4676698bcbc3
tam <tam@mikatosh.panter.local>
2009-11-12 15:18:57 +0100

    add all featues to tinymce

--- assets/javascripts/pol_backend.js
+++ assets/javascripts/pol_backend.js
@@ -26,12 +26,16 @@ function initRichEditors()
     editor_selector : "richtext",
     content_css : "/stylesheets/application.css",
     body_class : "comp",
-    theme_advanced_toolbar_location : "bottom",
-    theme_advanced_toolbar_align : "left",
-    plugins : "table",
-    theme_advanced_buttons3_add : "tablecontrols",
-    theme_advanced_buttons3_add_before : "tablecontrols,separator",
-    theme_advanced_buttons4 : "",
+    
+    plugins : "safari,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras",
+
+    // Theme options
+    theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
+    theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+    theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+    theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,pagebreak,|,insertfile,insertimage",
+    theme_advanced_statusbar_location : "bottom",
+    theme_advanced_resizing : true,
     removeformat_selector : 'p,b,strong,em,i,span,ins',
     table_inline_editing : true,
     external_image_list_url : "/ls-R.js?mode=external_image_list"
